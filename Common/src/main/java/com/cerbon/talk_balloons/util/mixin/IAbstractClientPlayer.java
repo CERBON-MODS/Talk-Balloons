48fc77e891a6239718b881baeb66d396f916d5de
Naz <bluspringindohq@gmail.com>
2024-12-23 05:51:03 +0800

    Use master branch for updating and publishing multiple versions at once

--- Common/src/main/java/com/cerbon/talk_balloons/util/mixin/IAbstractClientPlayer.java
+++
@@ -1,8 +0,0 @@
-package com.cerbon.talk_balloons.util.mixin;
-
-import com.cerbon.cerbons_api.api.general.data.HistoricalData;
-
-public interface IAbstractClientPlayer {
-    void createBalloonMessage(String text, int duration);
-    HistoricalData<String> getBalloonMessages();
-}
--- Forge/src/main/java/com/cerbon/talk_balloons/forge/event/TBEventsForge.java
+++
@@ -1,9 +0,0 @@
-package com.cerbon.talk_balloons.forge.event;
-
-import com.cerbon.talk_balloons.util.TBConstants;
-import net.minecraftforge.fml.common.Mod;
-
-@Mod.EventBusSubscriber(modid = TBConstants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class TBEventsForge {
-
-}
