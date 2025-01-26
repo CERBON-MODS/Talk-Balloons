749fd1b70512d530b52a691bac4403efae079c7b
Naz <bluspringindohq@gmail.com>
2025-01-26 23:48:41 +0800

    Fix #6, bump version to 1.1.2

--- common/src/main/java/com/cerbon/talk_balloons/mixin/ChatListenerMixin.java
+++ common/src/main/java/com/cerbon/talk_balloons/mixin/ChatListenerMixin.java
@@ -10,9 +10,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.PlayerChatMessage;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,15 +19,13 @@ import java.time.Instant;
 
 @Mixin(ChatListener.class)
 public class ChatListenerMixin {
-
-    @Shadow @Final private Minecraft minecraft;
-
     @Inject(method = "showMessageToPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;getChat()Lnet/minecraft/client/gui/components/ChatComponent;", ordinal = 0))
     private void getChatMessage(ChatType.Bound boundChatType, PlayerChatMessage chatMessage, Component decoratedServerContent, GameProfile gameProfile, boolean onlyShowSecureChat, Instant timestamp, CallbackInfoReturnable<Boolean> cir) {
-        Level level = this.minecraft.level;
+        Minecraft minecraft = Minecraft.getInstance();
+        Level level = minecraft.level;
         if (level == null) return;
 
-        Player thisClientPlayer = this.minecraft.player;
+        Player thisClientPlayer = minecraft.player;
         if (thisClientPlayer != null && thisClientPlayer.getUUID() == chatMessage.sender() && !TalkBalloons.config.showOwnBalloon) return;
 
         Player player = level.getPlayerByUUID(chatMessage.sender());
