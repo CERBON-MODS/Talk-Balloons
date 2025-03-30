974d36e24ca1e89c080668813492dcad5c9b9dff
Naz <bluspringindohq@gmail.com>
2025-03-30 14:52:21 +0800

    Fix rare CME with UnityTranslate

--- common/src/main/java/com/cerbon/talk_balloons/mixin/PlayerMixin.java
+++ common/src/main/java/com/cerbon/talk_balloons/mixin/PlayerMixin.java
@@ -16,8 +16,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
@@ -25,7 +26,7 @@ import java.util.function.Supplier;
 @SuppressWarnings("AddedMixinMembersNamePattern")
 public abstract class PlayerMixin extends LivingEntity implements IAbstractClientPlayer {
     @Unique private HistoricalData<String> balloonMessages;
-    @Unique private Set<Supplier<Boolean>> talk_balloons$queuedTickEvents = new HashSet<>();
+    @Unique private final Collection<Supplier<Boolean>> talk_balloons$queuedTickEvents = new ConcurrentLinkedDeque<>();
 
     protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level level) {
         super(entityType, level);
